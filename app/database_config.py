c56748873e0a92edd6564a1143ff22c8ad269d86
顾伟 <wadekoo321@gmail.com>
2025-04-11 17:43:25 +0800

    feat: 更新配置

--- app/database_config.py
+++ app/database_config.py
@@ -1,6 +1,5 @@
 """数据库配置模块，用于从环境变量获取数据库连接信息"""
 import os
-from urllib.parse import quote_plus
 
 def get_database_url():
     """从环境变量获取数据库URL"""
@@ -8,21 +7,26 @@ def get_database_url():
     if "SQLALCHEMY_DATABASE_URI" in os.environ:
         return os.environ["SQLALCHEMY_DATABASE_URI"]
     
-    # 从Zeabur的MySQL服务获取连接参数
-    if "MYSQL_HOST" in os.environ:
-        host = os.environ.get("MYSQL_HOST", "localhost")
-        port = os.environ.get("MYSQL_PORT", "3306")
-        user = os.environ.get("MYSQL_USER", "root")
-        password = os.environ.get("MYSQL_PASSWORD", "")
-        database = os.environ.get("MYSQL_DATABASE", "paraluxflow")
-        
-        # 对密码进行URL编码
-        encoded_password = quote_plus(password)
-        
-        return f"mysql+pymysql://{user}:{encoded_password}@{host}:{port}/{database}"
+    # 使用特定的Zeabur数据库配置
+    # 公共访问
+    PUBLIC_HOST = "sfo1.clusters.zeabur.com"
+    PUBLIC_PORT = "30465"
     
-    # 使用SQLite作为降级选项
-    return "sqlite:///instance/imp.db"
+    # 内部访问
+    INTERNAL_HOST = "mysql.zeabur.internal"
+    INTERNAL_PORT = "3306"
+    
+    # 优先使用内部连接（更安全更快）
+    host = INTERNAL_HOST
+    port = INTERNAL_PORT
+    
+    # 从环境变量获取用户名、密码和数据库名
+    user = os.environ.get("MYSQL_USER", "root")
+    password = os.environ.get("MYSQL_PASSWORD", "")
+    database = os.environ.get("MYSQL_DATABASE", "paraluxflow")
+    
+    # 不需要URL编码，Zeabur处理好了环境变量
+    return f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 def get_redis_url():
     """从环境变量获取Redis URL"""
